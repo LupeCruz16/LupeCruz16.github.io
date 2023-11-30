f479552befe6905bb2702d1bf5d3fd0ed6c09fd7
Guadalupe Cruz <113732018+LupeCruz16@users.noreply.github.com>
2023-11-30 20:11:00 +0000

    Landing Page Finished

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -6,26 +6,74 @@ import './css/constants.css'
 //cd my-portfolio
 //npm start
 function App() {
+  // Create state to manage hover style for individual links
+  const [hoveredLink, setHoveredLink] = React.useState(null);
+
+  // Event handlers for mouse enter and leave
+  const handleMouseEnter = (link) => {
+    setHoveredLink(link);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredLink(null);
+  };
+
+  // Function to determine style based on hovered link
+  const linkStyle = (link) => ({
+    transform: hoveredLink === link ? 'scale(1.1)' : 'none',
+    transition: 'transform 0.3s ease',
+  });
   return (
     <div className="App">
-      <header className="App-header">
 
-      <nav className="Navigation">
-        <a href="#home" className="Name-link">Lupe</a>
-        <div className="Nav-links">
-          <a href="#home">Home</a>
-          <a href="#projects">Projects</a>
-          <a href="#experience">Experience</a>
-          <a href="#contact">Contact</a>
-        </div>
-      </nav>
+      <header className="App-header">
+        <nav className="Navigation">
+          <a 
+            href="#home" 
+            className="Name-link" 
+            style={linkStyle('name')} 
+            onMouseEnter={() => handleMouseEnter('name')} 
+            onMouseLeave={handleMouseLeave}
+            >Guadalupe
+          </a>
+          <div className="Nav-links">
+            <a 
+              href="#home"
+              style={linkStyle('home')} 
+              onMouseEnter={() => handleMouseEnter('home')} 
+              onMouseLeave={handleMouseLeave} 
+              >Home
+            </a>
+            <a 
+              href="#projects" 
+              style={linkStyle('projects')} 
+              onMouseEnter={() => handleMouseEnter('projects')} 
+              onMouseLeave={handleMouseLeave}
+              >Projects
+            </a>
+            <a 
+              href="#experience" 
+              style={linkStyle('experience')} 
+              onMouseEnter={() => handleMouseEnter('experience')} 
+              onMouseLeave={handleMouseLeave}
+              >Experience
+            </a>
+            <a 
+              href="#contact" 
+              style={linkStyle('contact')} 
+              onMouseEnter={() => handleMouseEnter('contact')} 
+              onMouseLeave={handleMouseLeave}
+              >Contact
+            </a>
+          </div>
+        </nav>
 
         <div className = "App-title">
           <h1>Guadalupe Cruz</h1>
           <h2>Software Engineer</h2>
         </div>
-
       </header>
+
       <main className = "App-main">
         
       </main>
