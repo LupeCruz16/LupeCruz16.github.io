7b797168d4efe5354e9884904ea9afdafe287232
Guadalupe Cruz <113732018+LupeCruz16@users.noreply.github.com>
2023-12-01 05:45:14 +0000

    Changing background image

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import './css/constants.css'
+import headshotImage from './images/headshot.jpg'
+import awsLogo from './images/aws_logo.png'
 
 // Run using:
 //cd my-portfolio
@@ -23,26 +25,39 @@ function App() {
     transform: hoveredLink === link ? 'scale(1.1)' : 'none',
     transition: 'transform 0.3s ease',
   });
+
+  // Create state for Software Engineer effect
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    setFadeIn(true); // Trigger the fade-in effect on component mount
+  }, []);
+
+  // Create state for arrow effect
+  const [showArrow, setShowArrow] = useState(false);
+
+  useEffect(() => {
+    // Set a timeout to show the arrow after a few seconds
+    const timer = setTimeout(() => {
+      setShowArrow(true);
+    }, 2000);
+
+    // Clean up the timer when the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
 
       <header className="App-header">
         <nav className="Navigation">
-          <a 
-            href="#home" 
-            className="Name-link" 
-            style={linkStyle('name')} 
-            onMouseEnter={() => handleMouseEnter('name')} 
-            onMouseLeave={handleMouseLeave}
-            >Guadalupe
-          </a>
           <div className="Nav-links">
             <a 
-              href="#home"
-              style={linkStyle('home')} 
-              onMouseEnter={() => handleMouseEnter('home')} 
+              href="#aboutMe"
+              style={linkStyle('aboutMe')} 
+              onMouseEnter={() => handleMouseEnter('aboutMe')} 
               onMouseLeave={handleMouseLeave} 
-              >Home
+              >About Me
             </a>
             <a 
               href="#projects" 
@@ -50,13 +65,6 @@ function App() {
               onMouseEnter={() => handleMouseEnter('projects')} 
               onMouseLeave={handleMouseLeave}
               >Projects
-            </a>
-            <a 
-              href="#experience" 
-              style={linkStyle('experience')} 
-              onMouseEnter={() => handleMouseEnter('experience')} 
-              onMouseLeave={handleMouseLeave}
-              >Experience
             </a>
             <a 
               href="#contact" 
@@ -70,13 +78,55 @@ function App() {
 
         <div className = "App-title">
           <h1>Guadalupe Cruz</h1>
-          <h2>Software Engineer</h2>
+          <h2 className={fadeIn ? 'fade-in' : ''}>Software Engineer</h2>
         </div>
+        {showArrow && (
+          <div className="scroll-arrow">
+            ↓
+          </div>
+        )}
       </header>
 
       <main className = "App-main">
-        
+        <div className = "About-me">
+          <div className = "About-me-content">
+            <h1>About Me</h1>
+            <p>I am a Software Engineering student that has found a passion for programming.</p>
+            <p>Currently, my focus is on developing internal tools at SigParser, where I harness the capabilities of Large Language Models (LLMs) to enhance productivity and efficiency.</p>
+          </div>
+          <div>
+            <img src = {headshotImage} alt = "Guadalupes Headshot"></img>
+          </div>
+        </div>
+
+        <div className = "Projects">
+          <div className = "Current-project">
+            <h1>Projects</h1>
+            <h2>Current Focus</h2>
+            <p>Developing internal tools at <strong>SigParser</strong> by harnessing the capabilities of Large Language Models.</p>
+            <div className = "Current-project-skills">
+              <p>S3</p>
+              <p>Lambda</p>
+              <p>Langchain</p>
+            </div>
+          </div>
+          <div>
+            <img src = {awsLogo} alt = "Current Project" className = "spin-effect"></img>
+          </div>
+        </div>
+
+        <div className = "Contact">
+          <div className = "Contact-content">
+
+          </div>
+          <h2>Open to freelance opportunities</h2>
+          <p>Have an exciting project idea you need help with?</p>
+          <p>Send me an email!</p>
+
+          <p></p>
+        </div>
       </main>
+
       <footer>
         <p>© 2023 by Guadalupe Cruz. All rights reserved.</p>
       </footer>
