bd9755744cbe55f7f60e8f50d1af17757af84a72
Guadalupe Cruz <113732018+LupeCruz16@users.noreply.github.com>
2024-01-20 05:36:54 +0000

    Resimplifying navigation

--- my-portfolio/src/components/navigation/Navigation.js
+++ my-portfolio/src/components/navigation/Navigation.js
@@ -1,13 +1,9 @@
 import useHoverScale from '../../effects/useHoverScale.js';
-import { Link as RouterLink } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
 import { useState, useEffect } from 'react';
 import "./navigation.css"
 
 function Navigation() {
-    // Check if the current page is the home page
-    const isHomePage = window.location.pathname === '/';
-
     //Hover effect
     const { handleMouseEnter, handleMouseLeave, getScaleStyle } = useHoverScale();
     
@@ -49,24 +45,6 @@ function Navigation() {
         );
     };
 
-    // Used in instances when the user is not on home page
-    // gc directs the user to the home page
-    const pageLink = (to, label) => {
-        const isGcLink = label.toLowerCase() === 'gc';
-
-        return (
-            <RouterLink
-                to={isGcLink ? '/' : to}//key difference, directs to home page
-                className="navLinkElem textSizeS"
-                style={getScaleStyle(isGcLink ? 'gc' : to)}
-                onMouseEnter={() => handleMouseEnter(isGcLink ? 'gc' : to)}
-                onMouseLeave={handleMouseLeave}
-            >
-                {label}
-            </RouterLink>
-        );
-    };
-
     return (
         <div className = "paddingGlobal">
             <div className = "largeContainer">
@@ -74,32 +52,15 @@ function Navigation() {
                     <div className = "paddingGlobal">
                         <div className="navLinks">
                             <div className = "navLinks-Left">
-                                {isHomePage ? (
-                                    <>
-                                        {scrollLink('home', 'gc')}
-                                    </>
-                                ) : (
-                                    <>
-                                        {pageLink('/', 'gc')}
-                                    </>
-                                )}
+                                {scrollLink('home', 'gc')}
                             </div>
 
                             <div className = "navLinks-Right">
-                                {isHomePage ? (
-                                    <>
-                                        {scrollLink('home', 'Home')}
-                                        {scrollLink('aboutMe', 'About\u00A0Me')}
-                                        {scrollLink('projects', 'Projects')}
-                                        {scrollLink('testimonials', 'Testimonials')}
-                                        {scrollLink('contact', 'Contact')}
-                                    </>
-                                ) : (
-                                    <>
-                                        {pageLink('/', 'Home')}
-                                        {scrollLink('contact', 'Contact')}
-                                    </>
-                                )}
+                                {scrollLink('home', 'Home')}
+                                {scrollLink('aboutMe', 'About\u00A0Me')}
+                                {scrollLink('projects', 'Projects')}
+                                {scrollLink('testimonials', 'Testimonials')}
+                                {scrollLink('contact', 'Contact')}
                             </div>
                         </div>
                     </div>
