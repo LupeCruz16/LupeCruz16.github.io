c3f14e53c8bc4925ee44f8a556db0b99c840f4ce
Guadalupe Cruz <113732018+LupeCruz16@users.noreply.github.com>
2024-04-14 03:48:28 +0000

    Adding hero section of sigparser project info page

--- my-portfolio/src/assets/Images.js
+++ my-portfolio/src/assets/Images.js
@@ -4,9 +4,10 @@ import max from './images/max.jpg';
 import zoo from './images/zoo.jpg';
 import cat_display from './images/cat.jpg';
 import six from './images/six.jpg';
+import sigparser_logo from './images/sigparser.png'
 
 import headshot from './images/headshot.jpg';
 import pacificTech from './images/pacific_tech_client.jpg';
 
 export { museum, taylort_swift, max, zoo, cat_display, six,
-        headshot, pacificTech };
+        headshot, pacificTech, sigparser_logo };
