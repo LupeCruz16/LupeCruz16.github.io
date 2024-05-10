6495724ac37bf9dc430bc06da70044bb41da5e0f
Guadalupe Cruz <113732018+LupeCruz16@users.noreply.github.com>
2024-05-10 02:03:29 +0000

    Updating Home and Vice sections from SigParser page

--- my-portfolio/src/pages/sigparser/js/SigParser.js
+++ my-portfolio/src/pages/sigparser/js/SigParser.js
@@ -2,7 +2,8 @@ import Navigation from "../../../components/navigation/Navigation.js";
 import { toolHubSettings, toolHubVICE } from "../../../assets/Videos.js";
 import useFadeIn from '../../../effects/FadeIn/useFadeIn.js';
 import Footer from "../../../components/footer/Footer.js";
-import '../css/sigparser.css'
+import React, { useRef, useEffect } from 'react';
+import '../css/sigparser.css';
 
 const HeroSection = () => {
   return (
@@ -15,43 +16,11 @@ const HeroSection = () => {
   );
 };
 
-const SigParserHomeSection = () => {
-  return (
-    <div className = "vice-container">
-      <video autoPlay loop muted playsInline>
-        <source src= { toolHubSettings } type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div className = "text-muted text-s text-align-left">Phase 1: The Home Page</div>
-      <div className = "grid-1-3-col-container text-align-left">
-        <div></div>
-        <div className = "text-s">Our team focused on a simple but easy to manuver user interface.</div>
-      </div>
-    </div>
-  );
-};
-
-const ViceSection = () => {
-  return (
-    <div className = "vice-container">
-      <video autoPlay loop muted playsInline>
-        <source src= { toolHubVICE } type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div className = "text-muted text-s text-align-left">Phase 2: VICE</div>
-      <div className = "grid-1-3-col-container text-align-left">
-        <div></div>
-        <div className = "text-s">Chatbot specifics</div>
-      </div>
-    </div>
-  );
-};
-
 const OverviewSection = () => {
   return (
-    <div className = "project-num-hero-container text-align-left">
+    <div className = "sigparser-overview-container text-align-left">
       <div className = "grid-1-3-col-container">
-        <div className = "project-num-details" style={{ paddingTop: '12px' }}>
+        <div className = "sigparser-overview-details" style={{ paddingTop: '12px' }}>
           <div>
             <div className = "text-xs">Company</div>
             <div className = "text-muted text-s">SigParser</div>
@@ -65,19 +34,79 @@ const OverviewSection = () => {
             <div className = "text-muted text-s">2023-2024</div>
           </div>
         </div>
-        <div className = "project-num-details text-s">
+        <div className = "sigparser-overview-details text-s">
           <div>
             For my senior capstone project, I had the distinct opportunity to collaborate with SigParser. 
             Over the course of this year-long endeavor, my team and I developed a suite of internal LLM tools 
             designed to enhance operational efficiency and streamline workflows.
           </div>
           <div>
-            We adopted a robust <span className = "text-color-sec">CI/CD pipeline</span>, leveraging Amazon Bedrock services to ensure security 
+            We adopted a robust <span className = "underline text-muted">CI/CD pipeline</span>, leveraging Amazon Bedrock services to ensure security 
             while optimizing cost-effectiveness. This foundation enabled us to deploy a centralized 
-            platform, built with <span className = "text-color-sec">React</span>, that provided SigParser's staff with seamless access to manage and adapt 
+            platform, built with <span className = "underline text-muted">React</span>, that provided SigParser's staff with seamless access to manage and adapt 
             their tools effectively. This project not only honed my technical skills but also reinforced the 
-            importance of <span className = "text-color-sec">agile methodologies</span> in real-world software development.
+            importance of <span className = "underline text-muted">agile methodologies</span> in real-world software development.
           </div>        
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const HomeSection = () => {
+  return (
+    <div className = "sigparser-phase-container">
+      <video autoPlay loop muted playsInline>
+        <source src= { toolHubSettings } type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className = "text-muted text-s text-align-left">Phase 1: The Home Page</div>
+      <div className = "grid-1-3-col-container text-align-left">
+        <div></div>
+        <div className = "text-s">
+          The UI, built with React, prioritizes user experience. Each tool has customizable settings 
+          for modifying LLMs and, uniquely, our Chatbot (VICE) allows database updates. Transparent 
+          pricing is provided for developer convenience. This hub empowers developers to refine LLM 
+          tools for Sigparser's systems, with the Chatbot serving as a proof of concept.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const TechStack = () => {
+  return (
+    <div className = "sigparser-hero-container text-align-left">
+      <div className = "grid-1-3-col-container">
+        <div className = "text-xs" style={{ paddingTop: '12px' }}>Tech Stack</div>
+        <div className = "text-m">Technologies</div>
+      </div>
+    </div>
+  );
+};
+
+const ViceSection = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 1.5;
+    }
+  }, []);
+
+  return (
+    <div className = "sigparser-phase-container">
+      <video ref={videoRef} autoPlay loop muted playsInline>
+        <source src= { toolHubVICE } type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className = "text-muted text-s text-align-left">Phase 2: VICE</div>
+      <div className = "grid-1-3-col-container text-align-left">
+        <div></div>
+        <div className = "text-s">
+          VICE (Virtual Interface for Customer Enhancement) served as a playful 
+          acronym for our Chatbot. Leading its backend development, I orchestrated 
+          the integration with AWS, managed web scraping, and crafted conversation logic.
         </div>
       </div>
     </div>
@@ -92,8 +121,9 @@ function SigParser () {
       <Navigation />
       <div className = "content-grid">
         <HeroSection />
-        <SigParserHomeSection />
         <OverviewSection />
+        <HomeSection />
+        <TechStack />
         <ViceSection />
         <Footer />
       </div> 
