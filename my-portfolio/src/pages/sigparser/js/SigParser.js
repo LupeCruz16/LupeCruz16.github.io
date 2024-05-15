d7d96ce57ca26dabf1cbb50c122ba8dffe8086c1
Guadalupe Cruz <113732018+LupeCruz16@users.noreply.github.com>
2024-05-15 19:10:55 +0000

    Dine with SigParser page

--- my-portfolio/src/pages/sigparser/js/SigParser.js
+++ my-portfolio/src/pages/sigparser/js/SigParser.js
@@ -1,9 +1,9 @@
 import { FlaskSVGIcon, ReactSVGIcon, AmazonSVGIcon } from "../../../assets/SVGs.js";
-import { PythonSVGIcon, JavaScriptSVGIcon } from "../../../assets/SVGs.js";
 import { sigparser_home, vice_demo, query_demo } from "../../../assets/Videos.js";
 import Navigation from "../../../components/navigation/Navigation.js";
 import useFadeIn from '../../../effects/FadeIn/useFadeIn.js';
 import Footer from "../../../components/footer/Footer.js";
+import { PythonSVGIcon } from "../../../assets/SVGs.js";
 import { langchain } from "../../../assets/Images.js";
 import React, { useRef, useEffect } from 'react';
 import '../css/sigparser.css';
@@ -19,50 +19,13 @@ const HeroSection = () => {
   );
 };
 
-const OverviewSection = () => {
-  return (
-    <div className = "sigparser-overview-container text-align-left">
-      <div className = "grid-1-3-col-container">
-        <div className = "sigparser-overview-details" style={{ paddingTop: '12px' }}>
-          <div>
-            <div className = "text-xs">Company</div>
-            <div className = "text-muted text-s">SigParser</div>
-          </div>
-          <div>
-            <div className = "text-xs">Role</div>
-            <div className = "text-muted text-s">Developer</div>
-          </div>
-          <div>
-            <div className = "text-xs">Year</div>
-            <div className = "text-muted text-s">2023-2024</div>
-          </div>
-        </div>
-        <div className = "sigparser-overview-details text-s">
-          <div>
-            For my senior capstone project, I had the distinct opportunity to collaborate with SigParser. 
-            Over the course of this year-long endeavor, my team and I developed a suite of internal LLM tools 
-            designed to enhance operational efficiency and streamline workflows.
-          </div>
-          <div>
-            We adopted a robust <span className = "underline text-muted">CI/CD pipeline</span>, leveraging Amazon Bedrock services to ensure security 
-            while optimizing cost-effectiveness. This foundation enabled us to deploy a centralized 
-            platform, built with <span className = "underline text-muted">React</span>, that provided SigParser's staff with seamless access to manage and adapt 
-            their tools effectively. This project not only honed my technical skills but also reinforced the 
-            importance of <span className = "underline text-muted">agile methodologies</span> in real-world software development.
-          </div>        
-        </div>
-      </div>
-    </div>
-  );
-};
-
 const HomeSection = () => {
   return (
     <div className="sigparser-phase-container full-width">
       <div className="grid-1-1half-col-container">
         <div className="text-align-left">
-          <div className = "text-container">
-            <div className = "phase-1-description">
+          <div className = "phases-details-container">
+            <div className = "phase-1-and-3-description">
               <div className="text-muted text-m">Phase 1: The Home Page</div>
               <div className="text-s">
                 The UI, built with React, utilizes Flask for frontend and backend communication while 
@@ -91,24 +54,6 @@ const HomeSection = () => {
   );
 };
 
-const TechStack = () => {
-  return (
-    <div className = "sigparser-hero-container text-align-left" style={{ paddingTop: '0' }}>
-      <div className = "grid-1-3-col-container">
-        <div className = "text-muted text-s text-align-left">The Tech Stack</div>
-        <div className = "tech-stack">
-          <PythonSVGIcon />
-          <FlaskSVGIcon />
-          <img src = { langchain } alt = "Langchain"></img>
-          <AmazonSVGIcon/>
-          <ReactSVGIcon />
-          <JavaScriptSVGIcon />
-        </div>
-      </div>
-    </div>
-  );
-};
-
 const ViceSection = () => {
   const videoRef = useRef(null);
 
@@ -128,12 +73,12 @@ const ViceSection = () => {
           the integration with AWS, managed web scraping, and crafted conversation logic
           through Langchain.
         </div>
-        <div className = "sigparser-tech-graphics smaller-phase-graphics">
+        <div className = "sigparser-tech-graphics">
           <AmazonSVGIcon/>
           <img src = { langchain } alt = "Langchain"></img>
         </div>
       </div>
-      <video ref={videoRef} autoPlay loop muted playsInline className = "sigparser-phase-video">
+      <video ref={videoRef} autoPlay loop muted playsInline className = "sigparser-phase-2-video">
         <source src= { vice_demo } type="video/mp4" />
         Your browser does not support the video tag.
       </video>
@@ -143,19 +88,70 @@ const ViceSection = () => {
 
 const QueryGenSection = () => {
   return (
-    <div className = "sigparser-phase-container">
-      <video autoPlay loop muted playsInline className = "sigparser-phase-video">
-        <source src= { query_demo } type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div className = "sigparser-phase-title text-muted text-s text-align-left">Phase 3: The Query Generator</div>
-      <div className = "grid-1-3-col-container text-align-left">
-        <div></div>
-        <div className = "text-s">
-          For this tool I handled securley manaing login credentials and completed the UI. SigParser recognized 
-          that their existing filtering system was too complex and sought out an LLM based solution. As 
-          such, we implemented a tool with the capabilities of querying an LLM using a preformatted prompt
-          that would return a JSON payload that would integrate into their existing system seamlessly.
+    <div className="sigparser-phase-container full-width">
+      <div className="grid-1half-1-col-container">
+        <div className="video-container">
+          <video autoPlay loop muted playsInline className = "rounded-right">
+            <source src={query_demo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+
+        <div className="text-align-left">
+          <div className = "phases-details-container">
+            <div className = "phase-1-and-3-description">
+              <div className="text-muted text-m">Phase 3: The Query Generator</div>
+              <div className="text-s">
+                For this tool I handled securley manaing login credentials and completed the UI. SigParser recognized 
+                that their existing filtering system was too complex and sought out an LLM based solution. As 
+                such, we implemented a tool with the capabilities of querying an LLM using a preformatted prompt
+                that would return a JSON payload that would integrate into their existing system seamlessly.
+              </div>
+            </div>
+            <div className = "sigparser-tech-graphics">
+              <PythonSVGIcon />
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const OverviewSection = () => {
+  return (
+    <div className = "sigparser-overview-container text-align-left">
+      <div className= "text-muted text-m overview-title">Project Overview</div>
+      <div className = "grid-1-3-col-container">
+        <div className = "sigparser-overview-details" style={{ paddingTop: '12px' }}>
+          <div>
+            <div className = "text-xs">Company</div>
+            <div className = "text-muted text-s">SigParser</div>
+          </div>
+          <div>
+            <div className = "text-xs">Role</div>
+            <div className = "text-muted text-s">Developer</div>
+          </div>
+          <div>
+            <div className = "text-xs">Duration</div>
+            <div className = "text-muted text-s">2023-2024</div>
+          </div>
+        </div>
+        <div className = "sigparser-overview-details text-s">
+          <div>
+            For my senior capstone project, I had the distinct opportunity to collaborate with SigParser. 
+            Over the course of this year-long endeavor, my team and I developed a suite of internal LLM tools 
+            designed to enhance operational efficiency and streamline workflows.
+          </div>
+          <div>
+            We adopted a robust <span className = "underline text-muted">CI/CD pipeline</span>,  
+            leveraging <span className = "underline text-muted">Amazon Bedrock</span> services to ensure security 
+            while optimizing cost-effectiveness. This foundation enabled us to deploy a centralized 
+            platform, built with React, that provided SigParser's staff with seamless access to manage and adapt 
+            their tools effectively. This project not only honed my technical skills but also reinforced the 
+            importance of <span className = "underline text-muted">agile methodologies</span> in real-world software development.
+          </div>        
         </div>
       </div>
     </div>
@@ -171,7 +167,6 @@ function SigParser () {
       <div className = "content-grid">
         <HeroSection />
         <HomeSection />
-        <TechStack />
         <ViceSection />
         <QueryGenSection />
         <OverviewSection />
